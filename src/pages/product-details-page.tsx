758202ae980fa5200bbdd2f32e2445d351bfd57d
NatiCohen10 <nati1236nati@gmail.com>
2024-08-11 16:45:23 +0300

    fetched product from db

--- src/pages/product-details-page.tsx
+++ src/pages/product-details-page.tsx
@@ -1,4 +1,11 @@
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { formatPriceRange } from "@/lib/formatPriceRange";
 import { productService } from "@/services/proucts.service";
 import { IProduct } from "@/types/product.types";
 import { useQuery } from "@tanstack/react-query";
@@ -12,11 +19,31 @@ function ProductDetailsPage() {
     queryFn: () => productService.fetchProductById(productId!),
   });
 
-  return (
-    <Dialog open={true}>
-      <DialogContent aria-describedby={undefined}></DialogContent>
-    </Dialog>
-  );
+  if (data)
+    return (
+      <Dialog open={true}>
+        <DialogContent aria-describedby={undefined}>
+          <Card className=" min-h-full flex flex-col justify-between transition-all">
+            <CardHeader className=" items-center gap-4">
+              <div className="  rounded-sm bg-white w-full  ">
+                <img
+                  className=" mx-auto min-h-full p-2"
+                  src={data.img}
+                  alt={data.name}
+                />
+              </div>
+              <CardTitle className=" text-base">{data.name}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className=" text-xs text-muted-foreground">
+                Category: {data.category}
+              </p>
+              <p className=" text-xs">Price range: {formatPriceRange(data)}</p>
+            </CardContent>
+          </Card>
+        </DialogContent>
+      </Dialog>
+    );
 }
 
 export default ProductDetailsPage;
